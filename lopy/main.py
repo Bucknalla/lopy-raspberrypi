3aa004ca490da9c3302a55476f85a689cf80fd44
Alex Bucknall <alex@pycom.io>
2017-03-09 09:18:07 +0000

    Fixed any potential confusion
    Added new uart name, moved lora socket, added more sleep

--- lopy/main.py
+++ lopy/main.py
@@ -8,9 +8,11 @@ import struct
 
 pycom.heartbeat(False) # turn off heartbeat
 
-uart = UART(1, 115200, bits=8, parity=None, stop=1)
-uart.write("Connected...")
+uart1 = UART(1, 115200, bits=8, parity=None, stop=1)
+uart1.init(baudrate=115200, bits=8, parity=None, stop=1, timeout_chars=2, pins=("P3", "P4"))
+uart1.write("Connected...")
 
+#P3/P4
 # Initialize LoRa in LORAWAN mode.
 lora = LoRa(mode=LoRa.LORAWAN)
 app_eui = binascii.unhexlify('AD A4 DA E3 AC 12 67 6B'.replace(' ',''))
@@ -23,21 +25,22 @@ while not lora.has_joined():
 
 print("Connected to the network.")
 
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+s.setblocking(True)
+
 def send_lora(data):
     print("Sending " + data)
-    s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-    s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
-    s.setblocking(True)
     s.send(str(data))
-    s.setblocking(False)
-    uart.write(b'Success')
+    uart1.write(b'Success')
 
 while True:
-    if uart.any():
-        data = uart.readall()
+    if uart1.any():
+        data = uart1.readall()
         pycom.rgbled(0xFF0000) # set LED to RED on if data received
         if data == b'send':
             send_lora("data")
             pycom.rgbled(0x00FF00) # set LED to GREEN if data is b'send'
         time.sleep(1)
         pycom.rgbled(0x000000)
+    time.sleep(0.25)
